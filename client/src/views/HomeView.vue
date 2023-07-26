10029aad66921d48b620dafd511fb2654dbdad9f
Kaung Sint <kaungsintkoko99@gmail.com>
2023-07-26 20:11:13 +0630

    Revert "update published column in posts table to published_at and show datetime in published posts"
    This reverts commit ab4f0d8db6ca6acb2f021f298f343e4baaa52878.

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -15,14 +15,11 @@ const store = useStore()
 <template>
   <main class="space-y-4">
     <div v-for="post in posts" :key="post.uuid" class="shadow px-4 py-6 rounded">
-      <div class="flex items-center space-x-2 mb-2">
-        <h5 class="text-gray-500">{{ post.user.name }}</h5>
-        <time datetime="" class="text-xs">{{ post.published_at }}</time>
-      </div>
+      
         <RouterLink :to="{ name: 'post',  params: { slug: post.slug }}" class="">
-          <h1 class="text-2xl font-semibold mb-3 hover:underline duration-150">{{ post.title }}</h1>
+          <h1 class="text-2xl font-semibold mb-3">{{ post.title }}</h1>
+          <p class="text-sm text-gray-500">{{ post.excerpt }}</p>
         </RouterLink>
-          <p class="text-sm text-gray-500">{{ post.excerpt }}...</p>
     </div>
   </main>
 </template>
